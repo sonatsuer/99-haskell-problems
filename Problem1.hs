e46389be8e40a6e0c59049b049098966b102c3c2
Sonat Süer <sonatsuer@users.noreply.github.com>
2015-09-29 16:53:12 +0300

    Delete Problem1.hs

--- Problem1.hs
+++
@@ -1,9 +0,0 @@
-module Problem1 where
-
--- Problem 1
--- Find the last element of a list.
-
-myLast :: [a] -> a
-myLast [x]      = x
-myLast (_ : xs) = myLast xs
-myLast _        = error "The list should be nonempty."
