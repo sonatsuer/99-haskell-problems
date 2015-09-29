7dd79d917077f82c048c5b8853efe73d31345f27
Sonat Süer <sonatsuer@users.noreply.github.com>
2015-09-29 16:58:26 +0300

    Delete Problem2.hs

--- Problem2.hs
+++
@@ -1,9 +0,0 @@
-module Problem2 where
-
--- Problem 2
--- Find the last but one element of a list.
-
-myButLast :: [a] -> a
-myButLast [x, _]   = x
-myButLast (_ : xs) = myButLast xs
-myButLast _        = error "There should be at least 2 items in the list."
