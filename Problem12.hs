da0eafee27884fa652b232ee611b051d2784580f
sonatsuer <sonatsuer@gmail.com>
2015-09-30 14:17:29 +0300

    Minor tweak

--- Problem12.hs
+++ Problem12.hs
@@ -9,6 +9,6 @@ decodeModified :: Eq a => [MuSi a] -> [a]
 decodeModified [] = []
 decodeModified (Multiple n x : xs)
   | n == 2    = [x, x] ++ decodeModified xs
-  | otherwise = [x] ++ decodeModified (Multiple (n - 1) x : xs)
+  | otherwise = x : decodeModified (Multiple (n - 1) x : xs)
 decodeModified (Single x : xs)
-  = [x] ++ decodeModified xs
+  = x : decodeModified xs
